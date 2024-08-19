cb909f37320c3b479b02987f1017e613aa40d7f9
Aren <aren@arenm.me>
2024-08-19 22:08:35 +0200

    the thing

---
+++ src/components/yearly.tsx
@@ -0,0 +1,31 @@
+import {Input} from "@/components/ui/input";
+
+interface TaxProps {
+    boop: number;
+    bap: React.Dispatch<React.SetStateAction<number>>
+}
+
+export default function Yearly({bap, boop}: TaxProps) {
+
+    const apm = 0.7 * boop;
+    const gpm = apm * 0.05;
+    const vsd_psd_base = apm * 0.9
+    const vsd = vsd_psd_base * 0.1252;
+    const psd = vsd_psd_base * 0.0698;
+    const total = vsd + psd + gpm;
+
+    return (
+        <>
+            <div className={"justify-center align-middle items-center"}>
+                <Input value={boop} type={"number"} className={"w-40"} onInput={(event) => {
+                    bap(parseFloat(event.currentTarget.value))
+                }}  />
+            </div>
+
+            <p>gpm: {Math.round(gpm * 100) / 100}</p>
+            <p>vsd: {Math.round(vsd * 100) / 100}</p>
+            <p>psd: {Math.round(psd * 100) / 100}</p>
+            <p className="mt-3 text-2xl">total: {Math.round(total * 100) / 100}</p>
+        </>
+    )
+}
